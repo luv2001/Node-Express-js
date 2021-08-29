78cdaf37b83f8001a017c39d1ac70f093409596f
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-08-29 17:55:49 +0530

    __dirname __filename

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("New Course Starting")
+// console.log("Globals 🌍 --> no window  ")
+//  dirname filename require module process
+
+console.log(__dirname);
+console.log(__filename);
+
+// setTimeout(() => {
+//     console.log("Wait for me 2 second ⏳ ")
+// }, 2000)
+
+// setInterval(() => {
+//     console.log("Ye 1.2 sec pr aayega 🚀 ")
+// }, 1200)
